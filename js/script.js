740255a3659398ad6c08a6349c5819b6e2f54e56
Regina <regina-saifieva@mail.ru>
2018-10-21 18:11:27 +0300

    Add Timer

--- js/script.js
+++ js/script.js
@@ -33,4 +33,65 @@ info.addEventListener('click', function(event) {
 	}
 });
 
+let deadline = '2018-12-27';
+
+function getTimeRemaining(endtime) {
+	if (Date.parse(endtime) > Date.parse(new Date())) {
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			 	seconds = Math.floor((t / 1000) % 60),
+				minutes = Math.floor((t / 1000 / 60) % 60),
+				hours = Math.floor((t / (1000 * 60 * 60)));
+		if (hours < 10) {
+			hours = '0' + hours;
+		} if (minutes < 10) {
+			minutes = '0' + minutes;
+		} if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+		return {
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	} else {
+		let 	t = 0;
+		let seconds = 0,
+				minutes = 0,
+				hours = 0;
+		if (hours < 10) {
+			hours = '0' + hours;
+		}
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+		return {
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+}
+
+function setClock(id,endtime){
+	let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds');
+	var timeInterval = setInterval(updateClock,1000);
+function updateClock() {
+	let t = getTimeRemaining(endtime);
+	hours.textContent = t.hours;
+	minutes.textContent = t.minutes;
+	seconds.textContent = t.seconds;
+	if (t.total <= 0) {
+		clearInterval(timeInterval);
+	}
+}
+}
+setClock('timer', deadline);
 });
