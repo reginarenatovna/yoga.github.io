2bf7d318df359ad3956e6e36cfa72398f1909130
Regina <regina-saifieva@mail.ru>
2018-10-28 18:00:59 +0300

    change

--- src-js/parts/timer.js
+++ src-js/parts/timer.js
@@ -26,15 +26,16 @@ function timer() {
 			let t = 0;
 			let seconds = 0,
 				minutes = 0,
+				n = 0,
 				hours = 0;
 			if (hours < 10) {
-				hours = `{0 ${hours}}`;
+				hours = `${n} ${hours}`;
 			}
 			if (minutes < 10) {
-				minutes = `{0 ${minutes}}`;
+				minutes = `${n} ${minutes}`;
 			}
 			if (seconds < 10) {
-				seconds = `{0 ${seconds}}`;
+				seconds = `${n} ${seconds}`;
 			}
 			return {
 				'total': t,
